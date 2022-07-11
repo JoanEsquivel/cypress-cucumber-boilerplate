8feaec8a9e6019045391a1f121a2db6fe8e8e179
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-07-10 19:02:52 -0700

    cleaning html reports & renaming folder structure

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,16 +1,16 @@
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
-  jsonDir: "jsonreport", // ** Path of .json file **//
+  jsonDir: "jsonlogs", // ** Path of .json file **//
   reportPath: "./reports/cucumber-htmlreport.html",
   metadata: {
     browser: {
       name: "chrome",
-      version: "96",
+      version: "XX",
     },
     device: "Local test machine",
     platform: {
-      name: "MAC",
-      version: "Monterrey",
+      name: "Windows",
+      version: "11",
     },
   },
 });
