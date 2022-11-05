d1b747279338434fb5095cb46d0b24925fec4766
JoanEsquivel <joan.esquim@gmail.com>
2022-11-04 21:02:55 -0700

    Custom-command

--- cypress/e2e/step_definitions/login.js
+++ cypress/e2e/step_definitions/login.js
@@ -1,26 +1,28 @@
-import {
-  Given,
-  When,
-  Then,
-} from "@badeball/cypress-cucumber-preprocessor";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 const loginPage = require("../../pages/LoginPage");
 
 Given("A web browser is at the saucelabs login page", () => {
   cy.visit("/");
+  cy.customCommandTest();
 });
 
-When("A user enters the username {string}, the password {string}, and clicks on the login button", (username,password) => {
-  loginPage.submitLogin(username,password)
-});
+When(
+  "A user enters the username {string}, the password {string}, and clicks on the login button",
+  (username, password) => {
+    loginPage.submitLogin(username, password);
+  }
+);
 
-When("A user provides incorrect credentials, and clicks on the login button", (table) => {
-  table.hashes().forEach((row) => {
-    cy.log(row.username);
-    cy.log(row.password);
-    loginPage.submitLogin(row.username, row.password)
-
-  });
-});
+When(
+  "A user provides incorrect credentials, and clicks on the login button",
+  (table) => {
+    table.hashes().forEach((row) => {
+      cy.log(row.username);
+      cy.log(row.password);
+      loginPage.submitLogin(row.username, row.password);
+    });
+  }
+);
 Then("the url will contains the inventory subdirectory", () => {
   cy.url().should("contains", "/inventory.html");
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("customCommandTest", () => {
+  cy.log("I am a custom command");
+});
